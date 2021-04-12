7aacba3b0db73104585dcf22edfe596230d4048d
mattcroat <mattcroat.dev@gmail.com>
2021-04-12 21:52:22 +0200

    fix: Change port

--- server/server.js
+++ server/server.js
@@ -1,53 +1,52 @@
-const express = require("express");
-const swaggerJsDoc = require("swagger-jsdoc");
-const swaggerUI = require("swagger-ui-express");
-const cors = require("cors");
-const morgan = require("morgan");
-const low = require("lowdb");
-const FileSync = require("lowdb/adapters/FileSync");
-const booksRouter = require("./routes/books");
+const express = require('express')
+const swaggerJsDoc = require('swagger-jsdoc')
+const swaggerUI = require('swagger-ui-express')
+const cors = require('cors')
+const morgan = require('morgan')
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const booksRouter = require('./routes/books')
 
-const adapter = new FileSync("db.json");
-const db = low(adapter);
+const adapter = new FileSync('db.json')
+const db = low(adapter)
 
-db.defaults({ books: [] }).write();
+db.defaults({ books: [] }).write()
 
-const app = express();
+const app = express()
 
-app.db = db;
+app.db = db
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(morgan("dev"));
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(morgan('dev'))
 
 const options = {
-	definition: {
-		openapi: "3.0.0",
-		info: {
-			title: "Library API",
-			version: "1.0.0",
-			description:
-				"This is a simple Library CRUD API.",
-		},
-		servers: [
-			{
-				url: "http://localhost:3000",
-			},
-		],
-	},
-	apis: ["./routes/*.js"],
-};
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Library API',
+      version: '1.0.0',
+      description: 'This is a simple Library CRUD API.',
+    },
+    servers: [
+      {
+        url: 'http://localhost:4000',
+      },
+    ],
+  },
+  apis: ['./routes/*.js'],
+}
 
-const specs = swaggerJsDoc(options);
+const specs = swaggerJsDoc(options)
 app.use(
-	"/api-docs",
-	swaggerUI.serve,
-	swaggerUI.setup(specs, { explorer: true })
-);
+  '/api-docs',
+  swaggerUI.serve,
+  swaggerUI.setup(specs, { explorer: true })
+)
 
-app.use("/books", booksRouter);
+app.use('/books', booksRouter)
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4000
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.listen(port, () => console.log(`Server is running on port ${port}`))
